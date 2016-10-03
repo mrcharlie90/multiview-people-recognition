08344117b7b6d70fce0b3ff44c5396be3f43ef46
Mauro <mauro@mbp-di-mauro-2.dsl2750b>
2016-10-03 11:56:01 +0200

    Moved files into src

---
+++ src/pose_this.py
@@ -0,0 +1,13 @@
+print """\
+    Usage: thingy [OPTIONS]
+    -h                        Display this usage message
+    -H hostname               Hostname to connect to
+    """
+
+def fib(n):
+    print 'ciao ', n
+
+fib(10)
+
+
+fib
