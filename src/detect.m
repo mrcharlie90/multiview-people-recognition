6022ddc97ce12076687809a0c0f3a25a1c36e55c
Mauro <mauro@mbp-di-mauro-2.dsl2750b>
2016-10-04 14:26:21 +0200

    Script completed

--- src/detect.m
+++ src/detect.m
@@ -1,15 +1,16 @@
 % Compute the people detection in a set of images
 % images_path : path to the images
 % det_number : detection number
-function ret = detect(images_path, det_number)
-    
+function detect(images_path, det_number)
+    % Pre-trained models
     detectors = {
         '../toolbox/detector/models/AcfCaltech+Detector.mat' 
-        '../toolbox/detector/models/AcfInriaDetector.mat' 
+        '../toolbox/detector/models/AcfInriaDetector.mat' % <-- best
         '../toolbox/detector/models/LdcfCaltechDetector.mat' 
         '../toolbox/detector/models/LdcfInriaDetector.mat'
         };
     
+    % Detector loading
     detector_path = detectors{det_number};
     detector = load(detector_path);
     detector = detector.detector;
@@ -18,31 +19,60 @@ function ret = detect(images_path, det_number)
     imagefiles = dir([images_path '*.png']);      
     nfiles = length(imagefiles);
     
+    % Output dirs
+    video_out_path = [images_path 'video_out/'];
+    imgs_out_path = [images_path 'imgs_out/'];
+    if ~exist(video_out_path, 'dir') 
+        mkdir(video_out_path);
+    end
+    
+    if ~exist(imgs_out_path, 'dir') 
+        mkdir(imgs_out_path);
+    end
+    
     % Detect people
+    display('Detecting people...');
     for i=1:nfiles
         % Create a directory with the same 
         % filename of the image
         splits = strsplit(imagefiles(i).name, '.');
-        filename = splits{1};
-        dir_name = [images_path filename];
-        % mkdir(dir_name); % TODO: later use
+        file_name = splits{1};
+        
+        patches_dir = [imgs_out_path file_name '/'];
+        if ~exist(patches_dir, 'dir')
+            mkdir(patches_dir);
+        end
         
         % Read the image, detect people
         % and store the result in an image with 
         % *_dect.png as suffix
         img = imread([images_path imagefiles(i).name]);
         bbs = acfDetect(img, detector);
-        out_img = bbApply('embed', img, bbs, 'col', [0 255 0]);
-        figure(1)
-        imshow(out_img);
-        % bbApply('draw', bbs, 'g');
+        img_out = bbApply('embed', img, bbs, 'col', [0 255 0]);
         
-        k = waitforbuttonpress
+        % Write out the frame
+        frame_name = [video_out_path imagefiles(i).name];
+        imwrite(img_out, frame_name);
+       
+        % Crop patches
+        [patches, B] = bbApply('crop', img, bbs);
         
+        % Save each people detected in imgs_out folder with the relative
+        % coordinates
+        if ~isempty(B) && ~isempty(patches)
+            sz = size(B);
+            for i=1:sz(1)
+                current_B = B(i,:);
+                
+                mat_path = [patches_dir 'bb_' int2str(i) '.mat'];
+                save(mat_path, 'current_B');
+                
+                patch_path = [patches_dir 'patch_' int2str(i) '.png'];
+                imwrite(patches{i}, patch_path);
+            end
+        end
     end
     
-    %im(I); bbApply('draw', bbs, 'g');
-    % bbs = acfDetect(I, detector);
-    % [patches, B] = bbApply('crop', I, bbs)
+    display('done!');
 end
 
