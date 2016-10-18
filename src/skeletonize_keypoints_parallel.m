afcab46a564e445984870f46c35c29ffc0cfbcef
mr.charlie90@gmail.com <*G1thub3r$>
2016-10-18 11:54:32 +0200

    skeletonize keypoint parallel

---
+++ src/skeletonize_keypoints_parallel.m
@@ -0,0 +1,11 @@
+function skeletonize_keypoints_parallel(path, out_path)
+    if ~exist(path, 'dir')
+        display('Invalid input folder');
+    end
+    
+    if ~exist(out_path, 'dir')
+        display('Invalid output folder');
+    end
+    
+    
+end
